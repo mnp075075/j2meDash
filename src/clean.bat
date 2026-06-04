4cbc62c903bfd91a94432d6ea8227a514ed0be91
mnp075075 <mnp075075@gmail.com>
2026-06-04 18:16:05 +0700

    remade clean.bat

--- src/clean.bat
+++ src/clean.bat
@@ -14,7 +14,7 @@ if exist "j2meDash4+NOKIA.jar" (
 
 goto continue
 
-if not exist "j2meDash4.jar" "j2meDash8.jar" "j2meDash4+NOKIA.jar" ( goto failed )
+if not exist "j2meDash4.jar" if not exist "j2meDash8.jar" if not exist "j2meDash4+NOKIA.jar" ( goto failed1 )
 
 :continue
 cd classes
@@ -26,12 +26,16 @@ if exist *.class (
 )
 if not exist *.class ( 
 	cd ..
-	goto failed
+	goto failed2
 )
 
-:failed
-echo Cannot find *.jar and/or *.class
+:failed1
+echo Cannot find *.jar
 goto :eof
+
+:failed2
+echo Cannot find *.class
+goto eof
 
 :finished
 echo Finished deleting *.jar and *.class files
