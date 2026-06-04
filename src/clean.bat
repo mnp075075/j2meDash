db8aec191aee71b21410a83cc6dabd18b61d2823
mnp075075 <mnp075075@gmail.com>
2026-06-04 18:23:07 +0700

    reedited clean.bat again

--- src/clean.bat
+++ src/clean.bat
@@ -3,6 +3,18 @@
 echo Cleaning compiled .jar
 
 if exist "j2meDash4.jar" ( 
+	set found=1
+)
+if exist "j2meDash8.jar" (
+	set found=1
+)
+if exist "j2meDash4+NOKIA.jar" ( 
+	set found=1
+)
+
+if "%found%"==0 ( goto failed1 )
+
+if exist "j2meDash4.jar" ( 
 	del /f j2meDash4.jar 
 )
 if exist "j2meDash8.jar" (
@@ -13,8 +25,6 @@ if exist "j2meDash4+NOKIA.jar" (
 )
 
 goto continue
-
-if not exist "j2meDash4.jar" if not exist "j2meDash8.jar" if not exist "j2meDash4+NOKIA.jar" ( goto failed1 )
 
 :continue
 cd classes
@@ -35,7 +45,7 @@ goto :eof
 
 :failed2
 echo Cannot find *.class
-goto eof
+goto :eof
 
 :finished
 echo Finished deleting *.jar and *.class files
