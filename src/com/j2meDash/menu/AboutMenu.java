9170bde5bd2a95dad9586551653bc15fb5bc5016
mnp075075 <mnp075075@gmail.com>
2026-06-30 00:00:41 +0700

    new file trees

---
+++ src/com/j2meDash/menu/AboutMenu.java
@@ -0,0 +1,74 @@
+package com.j2meDash.menu;
+import com.j2meDash.main.*;
+import javax.microedition.lcdui.*;
+import javax.microedition.lcdui.game.*;
+
+/*
+
+ ██████╗ ██████╗  ██████╗ ███╗   ██╗     ██╗ █████╗ ██╗   ██╗ █████╗  ██╗██████╗  ██████╗ ████████╗██╗  ██╗
+██╔════╝ ██╔══██╗██╔═══██╗████╗  ██║     ██║██╔══██╗██║   ██║██╔══██╗███║██╔══██╗██╔═══██╗╚══██╔══╝██║  ██║
+██║  ███╗██║  ██║██║   ██║██╔██╗ ██║     ██║███████║██║   ██║███████║╚██║██║  ██║██║   ██║   ██║   ███████║
+██║   ██║██║  ██║██║   ██║██║╚██╗██║██   ██║██╔══██║╚██╗ ██╔╝██╔══██║ ██║██║  ██║██║   ██║   ██║   ╚════██║
+╚██████╔╝██████╔╝╚██████╔╝██║ ╚████║╚█████╔╝██║  ██║ ╚████╔╝ ██║  ██║ ██║██████╔╝╚██████╔╝   ██║        ██║
+ ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝ ╚════╝ ╚═╝  ╚═╝  ╚═══╝  ╚═╝  ╚═╝ ╚═╝╚═════╝  ╚═════╝    ╚═╝        ╚═╝
+
+*/
+
+public class AboutMenu extends GameCanvas {
+
+private MainApp mainApp;
+Display display = Display.getDisplay(mainApp);
+
+private MainMenu mainMenu = new MainMenu(mainApp);
+/* 
+ * the aboutMenu
+ * aboutMenu itself just tells the information about the game and its original creator
+ * shoutout to RobTop Games, better known as Robert Topala for originally making:
+ * GEOMETRY DASH (for PC and Mobile)
+ * also J2ME helped me make this project possible
+ */
+	
+	public AboutMenu(MainApp mainApp) {
+		super(true); // REQUIRED
+		
+		this.mainApp = mainApp;
+	}
+
+	// GUI FOR ABOUT MENU
+	public void paint(Graphics g) {
+		
+		g.setColor(0,173,119);
+		g.fillRect(0, 0, 240, 400);
+		g.setColor(255,255,255);
+		g.drawString("About Us", 10, 30, Graphics.LEFT | Graphics.TOP);
+		g.drawString("This game is inspired from:", 10, 60, Graphics.LEFT | Graphics.TOP);
+		g.drawString("Geometry Dash", 10, 75, Graphics.LEFT | Graphics.TOP );
+		g.drawString("Which is originally made by:", 10, 90, Graphics.LEFT | Graphics.TOP);
+		g.drawString("RobTop Games AB", 10, 105, Graphics.LEFT | Graphics.TOP);
+		g.drawString("This game is made possible using J2ME", 10, 135, Graphics.LEFT | Graphics.TOP);
+		g.drawString("Java and Java Micro Edition are", 10, 150, Graphics.LEFT | Graphics.TOP);
+		g.drawString("property of: Oracle Corporation", 10, 175, Graphics.LEFT | Graphics.TOP);
+		g.drawString("and Sun Microsystems", 10, 190, Graphics.LEFT | Graphics.TOP);
+
+		g.setColor(0,85,85);
+		g.drawRect(99, 249, 41, 31);
+		g.setColor(0, 206, 119);
+		g.fillRect(100, 250, 40, 30);
+		g.setColor(255,255,255);
+		g.drawString("OK", 120, 265, Graphics.HCENTER | Graphics.VCENTER);
+	
+	}
+	
+	// BUTTONS FOR ABOUT MENU
+	protected void pointerPressed(int x, int y) {
+		
+		if (x >= 100 && x <= 140 && y >= 250 && y <= 280) {
+			
+			display.setCurrent(mainMenu);
+			mainMenu.repaint();
+			
+		}
+		
+	}
+	
+}
---
+++ src/com/j2meDash/menu/SoundForm.java
@@ -0,0 +1,57 @@
+package com.j2meDash.menu;
+import com.j2meDash.main.*;
+import javax.microedition.lcdui.*;
+import javax.microedition.lcdui.game.*;
+
+/*
+
+ ██████╗ ██████╗  ██████╗ ███╗   ██╗     ██╗ █████╗ ██╗   ██╗ █████╗  ██╗██████╗  ██████╗ ████████╗██╗  ██╗
+██╔════╝ ██╔══██╗██╔═══██╗████╗  ██║     ██║██╔══██╗██║   ██║██╔══██╗███║██╔══██╗██╔═══██╗╚══██╔══╝██║  ██║
+██║  ███╗██║  ██║██║   ██║██╔██╗ ██║     ██║███████║██║   ██║███████║╚██║██║  ██║██║   ██║   ██║   ███████║
+██║   ██║██║  ██║██║   ██║██║╚██╗██║██   ██║██╔══██║╚██╗ ██╔╝██╔══██║ ██║██║  ██║██║   ██║   ██║   ╚════██║
+╚██████╔╝██████╔╝╚██████╔╝██║ ╚████║╚█████╔╝██║  ██║ ╚████╔╝ ██║  ██║ ██║██████╔╝╚██████╔╝   ██║        ██║
+ ╚═════╝ ╚═════╝  ╚═════╝ ╚═╝  ╚═══╝ ╚════╝ ╚═╝  ╚═╝  ╚═══╝  ╚═╝  ╚═╝ ╚═╝╚═════╝  ╚═════╝    ╚═╝        ╚═╝
+
+*/
+
+public class SoundForm extends Form implements CommandListener {
+	
+	private MainApp mainApp;
+	
+	public Command exitCommand = new Command("rage quit", Command.EXIT, 1);
+	public Command yesCommand = new Command("no yes", Command.OK, 1);
+	public Command noCommand = new Command("yes no", Command.CANCEL, 1);
+	public Command mainBS = new Command("main bullsh*t", Command.OK, 1);
+	
+	public SoundForm(CommandListener cl, MainApp mainApp) {
+		
+		super("# SOUNDWAVE #");
+		
+		this.mainApp = mainApp;
+		append("do you want sound?");
+		
+		addCommand(yesCommand);
+		addCommand(noCommand);
+		addCommand(mainBS);
+		
+		setCommandListener(this);
+		
+	}
+	
+	public void commandAction(Command c, Displayable d) {
+		if (c == mainBS) {
+			
+			mainApp.showMainMenu();
+			
+		} else if (c == yesCommand) {
+			
+			mainApp.showMainMenu();
+			
+		} else if (c == noCommand) {
+			
+			mainApp.showMainMenu();
+			
+		}
+	}
+	
+}
